86dac5b569d06fe2c024c07b6972f398d8dac145
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-10 13:18:45 -0400

    commit tree a447f583ffe1467800c0411d8e7081f108c62347

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(records){
+    for (const record of records) {
+        if (record.result === "W") {
+            return record.year;
+        }
+    }
+}
